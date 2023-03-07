b4ff3c2fb684d517c6f9b9047688f68ed246c8a2
jinkwui <kjk7179@gmail.com>
2023-03-08 07:17:17 +0900

    Update open_check.py
    [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed 방지

--- open_check.py
+++ open_check.py
@@ -1,16 +1,18 @@
 import urllib.request
 import urllib.parse
 import time
+import ssl
 
 def open_paju_check(url, json_data):
 
     counter = 0
+    context = ssl._create_unverified_context()
 
     while True:
         post_data = urllib.parse.urlencode(json_data).encode('utf-8')
         request = urllib.request.Request(url, post_data)
         request.add_header('Content-Type', 'application/json; charset=utf-8')
-        response = urllib.request.urlopen(request)
+        response = urllib.request.urlopen(request, context=context)
         body = response.read()  # return type bytes
 
         # 문자열에 포함되어 있는지 확인
